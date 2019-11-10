13bef4c9199062283e3185cbfec48d3bf87d5bd5
Jared Lunde <jared.lunde@gmail.com>
2019-11-09 19:33:30 -0700

    Adds hash option to server/writeStyles

--- server-src/index.js
+++ server-src/index.js
@@ -2,12 +2,9 @@ import fs from 'fs'
 import path from 'path'
 
 export const writeStyles = async (styles, outputPath, options = {}) => {
-  const {name, clearCache = true} = options
+  const {name, hash = styles.dash.hash, clearCache = true} = options
   const stylesString = styles.extract(clearCache)
-  const filename = path.join(
-    outputPath,
-    `${name || styles.dash.hash(stylesString)}.css`
-  )
+  const filename = path.join(outputPath, `${name || hash(stylesString)}.css`)
   await fs.promises.writeFile(filename, stylesString)
   return {filename, styles: stylesString}
 }
