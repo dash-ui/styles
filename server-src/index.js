376cfd9739112030013ec6750e2fb9831efecbf8
Jared Lunde <jared.lunde@gmail.com>
2019-11-09 19:41:20 -0700

    server/writeStyle returns a more sensible object now

--- server-src/index.js
+++ server-src/index.js
@@ -2,9 +2,10 @@ import fs from 'fs'
 import path from 'path'
 
 export const writeStyles = async (styles, outputPath, options = {}) => {
-  const {name, hash = styles.dash.hash, clearCache = true} = options
+  let {name, hash = styles.dash.hash, clearCache = true} = options
   const stylesString = styles.extract(clearCache)
-  const filename = path.join(outputPath, `${name || hash(stylesString)}.css`)
+  name = `${name || hash(stylesString)}.css`
+  const filename = path.join(outputPath, name)
   await fs.promises.writeFile(filename, stylesString)
-  return {filename, styles: stylesString}
+  return {filename, name, path: outputPath, styles: stylesString}
 }
